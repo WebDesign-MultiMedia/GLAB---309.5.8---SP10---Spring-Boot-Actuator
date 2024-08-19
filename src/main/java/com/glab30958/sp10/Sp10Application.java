ee11e94d4bc64ca777c5c5fd41b0ab70a98f7dfe
Julio <youremail@example.com>
2024-08-19 11:39:20 -0400

    Completed Glab 30958

---
+++ src/main/java/com/glab30958/sp10/Sp10Application.java
@@ -0,0 +1,13 @@
+package com.glab30958.sp10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = {"com.glab30958.sp10"})
+public class Sp10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sp10Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/glab30958/sp10/Sp10ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glab30958.sp10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sp10ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
